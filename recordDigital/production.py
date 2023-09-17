0bda57a08fa5c0022764c1f92044a72a49cb5c82
akande56 <abdulsalamabubakar52@gmail.com>
2023-09-17 14:21:46 +0100

    update

--- recordDigital/production.py
+++ recordDigital/production.py
@@ -46,8 +46,6 @@ DATABASES = {
         "DATABASE_URL",
     ),
 }
-# DATABASES["default"]["ATOMIC_REQUESTS"] = True
-# DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa: F405
 
 # URLS
 # ------------------------------------------------------------------------------
