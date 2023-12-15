4d0abe5c77a425c8dd1c9e510cfdf9af8ae87786
akande56 <abdulsalamabubakar52@gmail.com>
2023-12-15 21:04:30 +0100

    new db

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-#echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('bogo','bogo', 'abdul52.')"| python manage.py shell
+echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('bogo','bogo', 'abdul52.')"| python manage.py shell
